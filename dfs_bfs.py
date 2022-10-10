73d56590104b3c3af082b4c05185c41c1be1e852
kimsh2948 <shkim2948@gmail.com>
2022-10-11 03:13:45 +0900

    2022-10-11

--- dfs_bfs.py
+++ dfs_bfs.py
@@ -1,10 +1,45 @@
-count = int(input())
+from collections import deque
+import sys
 
-graph = []
-for i in range(count):
-    graph.append(list(map(int, input().split())))
+def bfs(graph, v, visited):
+    visited[v] = True
+    queue = deque([v])
 
-stack = []
+    while queue:
+        v = queue.popleft()
+        print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
 
-while graph:
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+n, m, v = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    v1, v2 = map(int, sys.stdin.readline().strip().split())
+    graph[v1].append(v2)
+    graph[v2].append(v1)
+
+for e in graph:
+    e.sort()
+
+visited = [False] * len(graph)
+
+dfs(graph, v, visited)
+print()
+
+visited = [False] * len(graph)
+
+bfs(graph, v, visited)
+
     
